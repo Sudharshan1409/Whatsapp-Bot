67b731e639a4e4a63488fda1d12c2355ae27ad54
Sudharshan V <sudarshan61kv@gmail.com>
2020-09-30 23:08:03 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 driver = webdriver.Chrome(os.path.join(os.getcwd(),'chromedriver'))
 print(os.path.join(os.getcwd(),'chromedriver'))
